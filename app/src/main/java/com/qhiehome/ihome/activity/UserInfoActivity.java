2e6dc1b7785abcf380774c8e2b42157125b161d7
xiangflight <xiangflight@foxmail.com>
2017-08-08 19:12:16 +0800

    fix(UserInfoActivity): reuse test number

--- app/src/main/java/com/qhiehome/ihome/activity/UserInfoActivity.java
+++ app/src/main/java/com/qhiehome/ihome/activity/UserInfoActivity.java
@@ -123,8 +123,8 @@ public class UserInfoActivity extends BaseActivity {
         mCurrentTime = System.currentTimeMillis();
         if (NetworkUtils.isConnected(this)) {
             ParkingOwnedService parkingOwnedService = ServiceGenerator.createService(ParkingOwnedService.class);
-            String phoneNum = SharedPreferenceUtil.getString(this, Constant.PHONE_KEY, "");
-            ParkingOwnedRequest parkingOwnedRequest = new ParkingOwnedRequest(phoneNum);
+            //String phoneNum = SharedPreferenceUtil.getString(this, Constant.PHONE_KEY, "");
+            ParkingOwnedRequest parkingOwnedRequest = new ParkingOwnedRequest(Constant.TEST_PHONE_NUM);
             Call<ParkingOwnedResponse> call = parkingOwnedService.parkingOwned(parkingOwnedRequest);
             call.enqueue(new Callback<ParkingOwnedResponse>() {
                 @Override
