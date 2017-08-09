44aab972168273fabccfb212dc59e8cf4f8345bb
xiangflight <xiangflight@foxmail.com>
2017-08-09 15:57:08 +0800

    feat(ParkingFragment): add topBar

--- app/src/main/java/com/qhiehome/ihome/activity/FeedbackActivity.java
+++ app/src/main/java/com/qhiehome/ihome/activity/FeedbackActivity.java
@@ -53,6 +53,7 @@ public class FeedbackActivity extends AppCompatActivity {
             actionBar.setDisplayShowTitleEnabled(false);
         }
         mTbFeedback.setTitle("意见反馈");
+        mTbFeedback.setTitleTextColor(getResources().getColor(R.color.white));
         mTbFeedback.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
