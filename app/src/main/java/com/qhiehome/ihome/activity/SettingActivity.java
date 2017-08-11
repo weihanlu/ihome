ebe59aaa8e83f36a140f81340ea256e580fdfd19
xiangflight <xiangflight@foxmail.com>
2017-08-11 22:58:55 +0800

    feat(SettingActivity): add check update module

--- app/src/main/java/com/qhiehome/ihome/activity/SettingActivity.java
+++ app/src/main/java/com/qhiehome/ihome/activity/SettingActivity.java
@@ -1,7 +1,11 @@
 package com.qhiehome.ihome.activity;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Environment;
 import android.support.annotation.NonNull;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBar;
@@ -13,6 +17,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 
+import com.afollestad.materialdialogs.DialogAction;
 import com.afollestad.materialdialogs.MaterialDialog;
 import com.qhiehome.ihome.R;
 import com.qhiehome.ihome.adapter.MeAdapter;
@@ -21,9 +26,19 @@ import com.qhiehome.ihome.manager.ActivityManager;
 import com.qhiehome.ihome.network.model.update.CheckUpdateResponse;
 import com.qhiehome.ihome.network.service.update.PgyService;
 import com.qhiehome.ihome.network.service.update.PgyServiceGenerator;
+import com.qhiehome.ihome.util.CommonUtil;
 import com.qhiehome.ihome.util.Constant;
+import com.qhiehome.ihome.util.LogUtil;
 import com.qhiehome.ihome.util.SharedPreferenceUtil;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
@@ -39,6 +54,8 @@ public class SettingActivity extends BaseActivity {
 
     private static final String TAG = "SettingActivity";
 
+    private static final String APK_UPDATE_URL = "http://www.pgyer.com/apiv1/app/install?aKey=2ecfc4108f814c51dabbb1b21c80fe90&_api_key=61bb58e6d87d6d2d6b84c7a44c237a7e&password=ihome";
+
     @BindArray(R.array.setting_menu)
     String[] mSettingMenu;
 
@@ -49,6 +66,12 @@ public class SettingActivity extends BaseActivity {
 
     @BindView(R.id.rv_setting)
     RecyclerView mRvSetting;
+
+    private boolean cancelUpdate;
+
+    MaterialDialog mDialog;
+
+    private String mSavedPath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -107,27 +130,31 @@ public class SettingActivity extends BaseActivity {
     }
 
     private void updateApp() {
-        // 1. request versionCode
-        //请求参数
-        Map<String, String> options = new HashMap<>();
-        options.put("aKey","2ecfc4108f814c51dabbb1b21c80fe90");
-        options.put("uKey","ce208a1437998ef0a8e5f60b7dc4aac1");
-        options.put("_api_key","61bb58e6d87d6d2d6b84c7a44c237a7e");
+        cancelUpdate = false;
+        // 1. request versionCode on server
+        // 2. get local app version
+        int serverCode = 2;
+        int versionCode = CommonUtil.getVersionCode();
+        if (serverCode > versionCode) {
+            mDialog = new MaterialDialog.Builder(this)
+                    .title("正在更新")
+                    .content("下载进度")
+                    .progress(false, 100, true)
+                    .dismissListener(new DialogInterface.OnDismissListener() {
+                        @Override
+                        public void onDismiss(DialogInterface dialog) {
+                            dialog.dismiss();
+                            cancelUpdate = true;
+                        }
+                    })
+                    .show();
+            // download apk
+            downloadApk();
+        }
+    }
 
-        PgyService pgyService = PgyServiceGenerator.createService(PgyService.class);
-        Call<CheckUpdateResponse> call = pgyService.checkUpdate(options);
-        call.enqueue(new Callback<CheckUpdateResponse>() {
-            @Override
-            public void onResponse(@NonNull Call<CheckUpdateResponse> call, @NonNull Response<CheckUpdateResponse> response) {
-
-            }
-
-            @Override
-            public void onFailure(@NonNull Call<CheckUpdateResponse> call, @NonNull Throwable t) {
-
-            }
-        });
-
+    private void downloadApk() {
+        new DownloadAsyncTask().execute(APK_UPDATE_URL);
     }
 
     private void initToolbar() {
@@ -150,6 +177,72 @@ public class SettingActivity extends BaseActivity {
     public static void start(Context context) {
         Intent intent = new Intent(context, SettingActivity.class);
         context.startActivity(intent);
+    }
+
+    private void installApk() {
+        File apkFile = new File(mSavedPath, "Ihome.apk");
+        if (!apkFile.exists()) {
+            return;
+        }
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setDataAndType(Uri.parse("file://" + apkFile.toString()), "application/vnd.android.package-archive");
+        startActivity(intent);
+    }
+
+    private class DownloadAsyncTask extends AsyncTask<String, Void, Void> {
+
+        @Override
+        protected Void doInBackground(String... params) {
+            try {
+                if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
+                    String sdpath = Environment.getExternalStorageDirectory() + "/";
+                    mSavedPath = sdpath + "download";
+                    URL url = new URL(params[0]);
+                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                    conn.connect();
+                    int length = conn.getContentLength();
+                    InputStream is = conn.getInputStream();
+                    File file = new File(mSavedPath);
+                    if (!file.exists()) {
+                        file.mkdir();
+                    }
+                    File apkFile = new File(mSavedPath, "Ihome.apk");
+                    FileOutputStream fos = new FileOutputStream(apkFile);
+                    int count = 0;
+                    byte buf[] = new byte[1024];
+                    do {
+                        int numRead = is.read(buf);
+                        count += numRead;
+                        final int progress = (int)(((float)count / length) * 100);
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                mDialog.setProgress(progress);
+                            }
+                        });
+                        if (numRead <= 0) {
+                            installApk();
+                            break;
+                        }
+                        fos.write(buf, 0, numRead);
+                    } while (!cancelUpdate);
+                    fos.close();
+                    is.close();
+                }
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    mDialog.dismiss();
+                }
+            });
+            return null;
+        }
+
     }
 
 }
--- app/src/main/java/com/qhiehome/ihome/util/CommonUtil.java
+++ app/src/main/java/com/qhiehome/ihome/util/CommonUtil.java
@@ -3,10 +3,14 @@ package com.qhiehome.ihome.util;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.graphics.Rect;
 import android.view.View;
 import android.view.ViewTreeObserver;
 import android.view.inputmethod.InputMethodManager;
+
+import com.qhiehome.ihome.application.IhomeApplication;
 
 public class CommonUtil {
 
@@ -30,6 +34,20 @@ public class CommonUtil {
     public static void toggleKeyboard(Context context) {
         InputMethodManager imm = (InputMethodManager) context.getSystemService(Context.INPUT_METHOD_SERVICE);
         imm.toggleSoftInput(0, InputMethodManager.HIDE_NOT_ALWAYS);
+    }
+
+    /**
+     * 当前应用的版本号
+     */
+    public static int getVersionCode() {
+        try {
+            PackageManager manager = IhomeApplication.getInstance().getPackageManager();
+            PackageInfo info = manager.getPackageInfo(IhomeApplication.getInstance().getPackageName(), 0);
+            return info.versionCode;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return 0;
+        }
     }
 
 }
